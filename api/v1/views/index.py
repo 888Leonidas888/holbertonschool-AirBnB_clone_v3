a2766f4ac081a4dc43208a104f6d5a66dcb4125f
Jhony <Jhonny14_1@hotmail.com>
2024-02-10 17:03:40 -0500

    feat: nuevo endpoint para consultar objetos por tipo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,20 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from console import classes
+from models import storage
+from models.base_model import BaseModel
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Nuestra primera salida exitosa con un JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def some_stats():
+    """Cuenta todas las clases por tipo."""
+    stats_class = {k: storage.count(
+        v) for k, v in classes.items() if v != BaseModel}
+    return jsonify(stats_class)
