2779c4a4b5abcb60ebea27a0e99f37612e42a4b0
Jhony <Jhonny14_1@hotmail.com>
2024-02-10 19:18:52 -0500

    feat: se gestiona un json para un 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +15,12 @@ app.register_blueprint(app_views)
 def close_session(close):
     """Cierra sesión"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Admnistra un respuesta de error 404"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
