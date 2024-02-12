076fe81001c850a32367e6176b8634c5d2550c24
Jhony <Jhonny14_1@hotmail.com>
2024-02-11 22:22:57 -0500

    chore: se modifica CORS

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """Creamos nuestros Blueprint!!!"""
 
 
+from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 """Esta línea siguiente evitara de errores de importación circulante"""
 from api.v1.views.places import *
@@ -9,4 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
-from flask import Blueprint
